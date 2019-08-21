7fd635fd05bf26b1af13aa3e4a6b508f6125ac01
andrius-butent <andrius.antipovas@gmail.com>
2019-08-21 23:13:28 +0300

    Added constants

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,13 @@
 package com.gildedrose;
 
 class GildedRose {
+
+    private static final String AGE_BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String CONJURED = "Conjured Mana Cake";
+    private static final String CUSTOM_ITEM = "Custom Item";
+    
     Item[] items;
 
     public GildedRose(Item[] items) {
